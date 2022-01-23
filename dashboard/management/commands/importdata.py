5a5adcf7119b70f224aaa58ccd2e02c07d6a0d1b
AdithyaKrishnaK <vineetha.kadithya2001@gmail.com>
2022-01-24 00:07:20 +0530

    Migrate to NAS script, Healpix implemented on import data

--- dashboard/management/commands/importdata.py
+++ dashboard/management/commands/importdata.py
@@ -1,3 +1,4 @@
+from email.headerregistry import HeaderRegistry
 from genericpath import isdir
 from math import nan
 from django.core.management.base import BaseCommand
@@ -5,6 +6,9 @@ from dashboard.models import Image
 import datetime, pytz
 import os, warnings
 import astropy.io.fits
+import numpy as np
+import healpy as hp
+import astropy.wcs as wcs
 
 class Command(BaseCommand):
 
@@ -15,49 +19,55 @@ class Command(BaseCommand):
         parser.add_argument('--day',help="Add all images from a day")
         parser.add_argument('-i', nargs=3, help="Find folders of days and add them. Provide path, startdate, enddate in the format yyyymmdd")
         parser.add_argument('--header',help='Add custom header (-RA.wcs.proc.header.fits) files rather than default')
+        parser.add_argument('--migrate', nargs=2 ,help='Migrate file path to NAS. Inputs - date in format yyyymmdd and new path including the folder name')
+    
 
     def handle(self, *args, **options):
-        counts = 0
-        counte = 0
+        count_s = 0
+        count_err = 0
         countedit = 0
         
         add = options['add']
-        edit = options['edit']
         folder = options['dir']
         day = options['day']
         bulk = options['i']
         headerfile = options['header']
+        migrateData = options['migrate']
 
-        if headerfile=='True':
-            headerfile = True
-        elif headerfile=='False':
-            headerfile = False
+        if headerfile != None:
+            if headerfile=='True':
+                headerfile = True
+            elif headerfile=='False':
+                headerfile = False
+            else:
+                self.stdout.write(self.style.ERROR("Unknown argument for --header"))
+                return
         else:
-            self.stdout.write(self.style.ERROR("Unknown argument for header"))
-            return
+            headerfile = False
+        
 
-        if add != None and folder == None and day == None and bulk == None and edit == None:
+        if add != None and folder == None and day == None and bulk == None and migrateData == None:
             if not os.path.exists(add):
                 self.stdout.write(self.style.ERROR("Path does not exist"))
                 return
             res = self.addImage(add)
             if res == 0:
-                counte = counte+1
+                count_err = count_err+1
             elif res == 2:
-                counts = counts+1
+                count_s = count_s+1
             elif res == 1:
                 countedit = countedit+1
-        elif add == None and folder != None and day == None and bulk == None and edit == None:
+        elif add == None and folder != None and day == None and bulk == None and migrateData == None:
             if not os.path.exists(folder):
                 self.stdout.write(self.style.ERROR("Path does not exist"))
                 return
-            [counts,counte,countedit] = self.addImages(folder,headerfile)   
-        elif add == None and folder == None and day != None and bulk == None and edit == None:
+            [count_s,count_err,countedit] = self.addImages(folder,headerfile)   
+        elif add == None and folder == None and day != None and bulk == None and migrateData == None:
             if not os.path.exists(day):
                 self.stdout.write(self.style.ERROR("Path does not exist"))
                 return
-            [counts,counte, countedit] = self.addDay(day,headerfile)
-        elif add == None and folder == None and day == None and bulk != None and edit == None:
+            [count_s,count_err, countedit] = self.addDay(day,headerfile)
+        elif add == None and folder == None and day == None and bulk != None and migrateData == None:
             # import all data
             day1 = bulk[1]
             day2 = bulk[2]
@@ -76,30 +86,25 @@ class Command(BaseCommand):
                     print(name)
                     if os.path.exists(os.path.join(PATH,name)):
                         [success,fail,edit] = self.addDay(os.path.join(PATH,name),headerfile)
-                        counts = counts + success
-                        counte = counte + fail
+                        count_s = count_s + success
+                        count_err = count_err + fail
                         countedit = countedit + edit
                     else:
                         self.stdout.write(self.style.WARNING("Folder of %s does not exists"%name))
 
                     start = start + datetime.timedelta(days=1)
                 
-        elif add == None and folder == None and day == None and bulk == None and edit != None:
-            res = self.editImage(edit)
-            if res == 0:
-                counte = counte+1
-            elif res == 2:
-                counts = counts+1
-            elif res == 1:
-                countedit = countedit+1
+        elif add == None and folder == None and day == None and bulk == None and migrateData != None:
+            self.migrateDataNAS(migrateData[0],migrateData[1])
 
-        self.stdout.write(self.style.SUCCESS("Added %s entries"%counts))
-        self.stdout.write(self.style.SUCCESS("Updated %s entries"%countedit))
-        self.stdout.write(self.style.WARNING("Unable to add %s entries"%counte))
+        if migrateData == None:
+            self.stdout.write(self.style.SUCCESS("Added %s entries"%count_s))
+            self.stdout.write(self.style.SUCCESS("Updated %s entries"%countedit))
+            self.stdout.write(self.style.WARNING("Unable to add %s entries"%count_err))
 
     def addDay(self,PATH,h):
-        counts = 0
-        counte = 0
+        count_s = 0
+        count_err = 0
         countedit = 0 
         ignore = ['fig','files','flat','test','bias']
         targets = [name for name in os.listdir(PATH) if  not name in ignore and isdir(os.path.join(PATH,name))]
@@ -109,40 +114,41 @@ class Command(BaseCommand):
                 url = os.path.join(PATH,folder,'reduced')
             else:
                 print("Reduced folder does not exist in ",folder)
-                return [counts, counte, countedit]
+                return [count_s, count_err, countedit]
             [success,fail,edit] = self.addImages(url, h)
-            counts = counts + success
-            counte = counte + fail
+            count_s = count_s + success
+            count_err = count_err + fail
             countedit = countedit + edit
-        return [counts, counte, countedit]
+        return [count_s, count_err, countedit]
 
     def addImages(self,PATH, h):
-        counts = 0
-        counte = 0
+        count_s = 0
+        count_err = 0
         countedit = 0
         extension_name = '-RA.wcs.proc.fits' if not h else '-RA.wcs.proc.header.fits'
         images = [name for name in os.listdir(PATH) if name.endswith(extension_name)]
         for image in images:
-            res = self.addImage(os.path.join(PATH,image))
+            res = self.addImage(os.path.join(PATH,image),h)
             if res == 0:
-                counte = counte+1
+                count_err = count_err+1
             elif res == 2:
-                counts = counts+1
+                count_s = count_s+1
             elif res == 1:
                 countedit = countedit+1
             
 
-        return [counts,counte,countedit]
+        return [count_s,count_err,countedit]
 
-    def addImage(self,PATH):
-        if Image.objects.filter(filepath=PATH):
-            return 1
+    def addImage(self,PATH,h):
+        NSIDE = 64 # for healpix
+
         headers = Image.headers
         cols = headers.keys()
         fits = astropy.io.fits.open(PATH)
         hdu = fits[0]
         obj = {}
         for col in cols:
+            # convert and store datetime
             if col=='date_observed':
                 warnings.simplefilter("ignore")
                 try:
@@ -159,13 +165,51 @@ class Command(BaseCommand):
                 obj[col] = hdu.header[headers[col]]
             except:
                 obj[col] = nan
-        obj['filepath'] = os.path.abspath(PATH)
+
+        # Healpix        
+        try:
+            w = wcs.WCS(hdu.header)
+        except (KeyError,ValueError):
+            w = None
+            self.stdout.write(self.style.WARNING("Unable to read WCS for %s"%PATH))
+
+        if w!=None:
+            try:
+                center_x = hdu.header['NAXIS1']/2
+                center_y = hdu.header['NAXIS2']/2
+                [ra, dec] = w.wcs_pix2world(center_x,center_y,1)
+                obj['healpy_pxl'] = hp.pixelfunc.ang2pix(NSIDE,theta=np.deg2rad(dec),phi=np.deg2rad(ra), lonlat=True)
+            except Exception as e:
+                self.stdout.write(self.style.ERROR(e))
+                print("file name %s"%PATH)
+        # if difference file exists
+        [head, tail] = os.path.split(PATH)
+        diff_files = [name for name in os.listdir(head) if name.endswith('.diff')]
+        file_name = tail.split('-RA')
+        for file in diff_files:
+            if file.startswith(file_name[0]):
+                obj['diff_exists'] = 'yes'
+        if obj.get('diff_exists')== None:
+            obj['diff_exists'] = 'no'
+
+        # for special header files
+        if h:
+            [head, tail] = os.path.split(PATH)
+            new_file_name = tail.replace('-RA.wcs.proc.header.fits','-RA.wcs.proc.fits')
+            new_path = os.path.join(head,new_file_name)
+            if os.path.exists(new_path):
+                obj['filepath'] = os.path.abspath(new_path)
+            else:
+                self.stdout.write(self.style.ERROR("Image file does not exist for %s"%PATH))
+                return 0
+        else:
+            obj['filepath'] = os.path.abspath(PATH)
         try:
             dbimage = Image(**obj)
             record = Image.objects.filter(filepath=os.path.abspath(PATH))
             if record.exists():
-                record = dbimage
-                record.save()
+                record.delete()
+                dbimage.save()
                 return 1
             else:
                 dbimage.save()
@@ -176,47 +220,22 @@ class Command(BaseCommand):
             print("file name %s"%PATH)
             return 0
 
-    def editImage(self,PATH):
-        headers = Image.headers
-        cols = headers.keys()
-        fits = astropy.io.fits.open(PATH)
-        hdu = fits[0]
-        obj = {}
-        for col in cols:
-            if col=='date_observed':
-                warnings.simplefilter("ignore")
-                try:
-                    d = datetime.datetime.strptime(hdu.header[headers[col]], "%Y-%m-%dT%H:%M:%S.%f" )
-                except:
-                    try:
-                        d = datetime.datetime.strptime(hdu.header[headers[col]], "%Y-%m-%d %H:%M:%S.%f" )
-                    except:
-                        pass
-
-                d = pytz.timezone('UTC').localize(d)
-                obj[col] = d
+    def migrateDataNAS(self,date,newpath):
+        images = Image.objects.filter(filepath__contains='/'+date+'/')
+        num_images = len(images)
+        success = 0
+        for image in images:
             try:
-                obj[col] = hdu.header[headers[col]]
+                tail = image.filepath.split('/'+date+'/')[1]
+                PATH = os.path.join(newpath,date,tail)
+                if os.path.exists(PATH):
+                    image.filepath = os.path.abspath(PATH)
+                    image.save()
+                    success += 1
             except:
-                obj[col] = nan
-        obj['filepath'] = PATH
-        try:
+                pass
+        
 
-            if Image.objects.filter(filepath=PATH):
-                dbimage = Image(**obj)
-                record = Image.objects.filter(filepath=PATH)
-                record = dbimage
-                record.save()
-                dbimage.save()
-            else:
-                self.stdout.write(self.style.ERROR("Record does not exist, Did you mean add?"))
-            return 2
-        except Exception as e:
-            self.stdout.write(self.style.ERROR("Unable to edit"))
-            print(e)
-            print("file name %s"%PATH)
-            clash = Image.objects.filter(jd=obj['jd'])
-            for i in clash:
-                print(i.filepath)
-            return 0
-
+        self.stdout.write(self.style.SUCCESS("Updated %s entries"%success))
+        self.stdout.write(self.style.ERROR("Unable to update %s entries"%(num_images-success)))
+        
--- dashboard/models.py
+++ dashboard/models.py
@@ -9,50 +9,50 @@ class Proposal(models.Model):
 class Image(models.Model):
     jd = models.FloatField(blank=False)
     date_observed = models.DateTimeField(auto_now=False,auto_now_add=False) 
-    exposure = models.FloatField(null=True)
-    tar_ra = models.FloatField(null=True)
-    tar_dec = models.FloatField(null=True)
-    tar_name = models.CharField(max_length=20)
+    exposure = models.FloatField(null=True, blank=True)
+    tar_ra = models.FloatField(null=True, blank=True)
+    tar_dec = models.FloatField(null=True, blank=True)
+    tar_name = models.CharField(max_length=20, blank=True)
 
 
-    ccd_temp = models.FloatField(null=True)
-    tel_alt = models.FloatField(null=True)
-    tel_az = models.FloatField(null=True)
-    air_mass = models.FloatField(null=True)
-    hour_angle = models.FloatField(null=True)
-    avg = models.FloatField(null=True)
-    stdev = models.FloatField(null=True)
-    filter_used = models.CharField(max_length=20,null=True)
-    image_type = models.CharField(max_length=20,null=True) 
-    focus_value = models.CharField(max_length=20,null=True)
-    ra_rate = models.FloatField(null=True)
-    dec_rate = models.FloatField(null=True)
-    proposal_no = models.CharField(max_length=20,null=True)
-    progid = models.CharField(max_length=20,null=True)
-    pi = models.CharField(max_length=20,null=True)
-    tile_id = models.FloatField(null=True)
+    ccd_temp = models.FloatField(null=True, blank=True)
+    tel_alt = models.FloatField(null=True, blank=True)
+    tel_az = models.FloatField(null=True, blank=True)
+    air_mass = models.FloatField(null=True, blank=True)
+    hour_angle = models.FloatField(null=True, blank=True)
+    avg = models.FloatField(null=True, blank=True)
+    stdev = models.FloatField(null=True, blank=True)
+    filter_used = models.CharField(max_length=20,null=True, blank=True)
+    diff_exists = models.CharField(max_length=3,null=True, blank=True) 
+    focus_value = models.CharField(max_length=20,null=True, blank=True)
+    ra_rate = models.FloatField(null=True, blank=True)
+    dec_rate = models.FloatField(null=True, blank=True)
+    proposal_no = models.CharField(max_length=20,null=True, blank=True)
+    progid = models.CharField(max_length=20,null=True, blank=True)
+    pi = models.CharField(max_length=20,null=True, blank=True)
+    tile_id = models.FloatField(null=True, blank=True)
     
     
-    fwhm = models.FloatField(null=True)
-    lim_mag = models.FloatField(null=True)
-    lim_mag3 = models.FloatField(null=True)
-    psf_mag = models.FloatField(null=True)
-    psf_merr = models.FloatField(null=True)
-    psf_type = models.CharField(max_length=20,null=True)
-    psf_zerr = models.FloatField(null=True)
-    psf_zp = models.FloatField(null=True)
+    fwhm = models.FloatField(null=True, blank=True)
+    lim_mag = models.FloatField(null=True, blank=True)
+    lim_mag3 = models.FloatField(null=True, blank=True)
+    psf_mag = models.FloatField(null=True, blank=True)
+    psf_merr = models.FloatField(null=True, blank=True)
+    psf_type = models.CharField(max_length=20,null=True, blank=True)
+    psf_zerr = models.FloatField(null=True, blank=True)
+    psf_zp = models.FloatField(null=True, blank=True)
 
     filepath = models.CharField(max_length=120, unique=True, blank=False)
 
-    ref_ra =  models.FloatField(null=True)
-    ref_dec = models.FloatField(null=True)
+    ref_ra =  models.FloatField(null=True, blank=True)
+    ref_dec = models.FloatField(null=True, blank=True)
 
-    boundry_points = models.CharField(max_length=120,blank=True,null=True)
+    healpy_pxl = models.IntegerField(null=True, blank=True)
 
     headers = {
             "jd" :'JD',
             "date_observed":"DATE-OBS",
-            'exposure' : 'EXPOSURE',
+            'exposure' : 'EXPTIME',
             'tar_name' : 'OBJECT',
             'ccd_temp' : 'CCD_TEMP',
             'tel_alt' : 'TEL_ALT', 
@@ -63,7 +63,6 @@ class Image(models.Model):
             'stdev': 'STDEV',
             'filter_used' : 'FILTER',
             'focus_value' : 'FOCUSER',
-            'image_type' : 'IMAGETYP',
             'ra_rate':'RA-RATE',
             'dec_rate':'DEC-RATE',
             'proposal_no' :'PROPNUMS',
